7432a244bfd8a55081dc4672df862e85c00ad84d
viorelrotari15 <viorelrotari15@gmail.com>
2019-11-24 22:13:56 +0200

    this is solution to the 2nd task

---
+++ index.js
@@ -0,0 +1,47 @@
+function newPromise() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const randomBoolean = Math.round(Math.random() * 10) % 2 === 0;
+        return resolve(randomBoolean);
+      }, 1000);
+    });
+  };
+  
+  newPromise();
+   
+  
+
+   
+  function myfunc (value){
+      results.response.push(value)
+      if (value==true){
+          results._true++
+      }
+      else (value==false){
+          results._false++
+      }
+      console.log(value )
+      console.log(results)
+      console.log( "nr true",results)
+      console.log( "nr false",results)
+  }
+ 
+  var interator=0;
+  
+  results
+  
+  
+  while (interator<10 ){
+    newPromise().then(myfunc ) ;
+    interator++
+
+
+}
+var results = {
+    response: [],
+    _true: 0,
+    _false: 0
+     }
+
+     JSON.stringify(results);
+     localStorage.setItem('viorel', results);
